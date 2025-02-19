86385b1c91d06f15b8b957f9e0868f7b8ae5c8c2
zegganewalid <zegganewalid11@gmail.com>
2025-02-19 09:24:01 +0100

    j

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),vue()],
   base: process.env.NODE_ENV === 'production' 
-    ? '/n/VueJsSpaceX//' // Remplacez par le nom de votre dépôt GitHub
+    ? '/n/VueJsSpaceX//'
     : '/',
 })
